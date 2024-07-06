388022ee21aa2758698f7d07c173b8fdd21ca0e8
Ghiancarlo Morelli <104920272+GBDM97@users.noreply.github.com>
2024-07-05 23:39:25 -0300

    searching weekly assets

--- b3Reader.py
+++ b3Reader.py
@@ -1,16 +1,17 @@
 import re
 
 def formatPrice(s):
-    return str(int(re.sub(r'^0+', '', s))/100)
+    return str(int(re.sub(r'^0+', '', s.strip()))/100)
 
 while True:
     selected_asset = input('Search asset:\n').upper()
     with open('D24062024.txt') as file:
-        for i,d in enumerate(file):
+        linesArray = file.readlines()
+        for i,d in enumerate(linesArray):
             current_asset = d[12:24].strip()
-            if i == 0:
+            if i == 0 or i == len(linesArray)-1:
                 continue
-            if selected_asset in current_asset:
+            if selected_asset in current_asset and len(current_asset) > 8:
                 print('\n'+current_asset+' >>> '+
                         formatPrice(d[56:69])+'|'+formatPrice(d[69:82])+'|'+
                         formatPrice(d[82:95])+'|'+formatPrice(d[108:121]))
