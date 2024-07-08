73cc9a9bc34bfe28d612f3b05d790019eebf1769
Ghiancarlo Morelli <104920272+GBDM97@users.noreply.github.com>
2024-07-08 20:48:47 -0300

    txtToJson is functional

--- PythonScripts/txtToJson.py
+++ PythonScripts/txtToJson.py
@@ -4,15 +4,19 @@ from pathlib import Path
 import b3Reader
 
 currentJsonKeys = []
+dataFileContent = ''
 with open('ReactApp//optionsreplay//src//data//data.json') as file:
-    currentJsonKeys = list(json.loads(file.read()).keys())
+    dataFileContent = json.loads(file.read())
+    currentJsonKeys = list(dataFileContent.keys())
 currentFiles = [file.name.replace('.txt','') for file in Path('PythonScripts//data').iterdir()]
 
-def jsonWrite(processedData):
-    print(processedData)
+def jsonWrite(dateCode,processedData):
+    dataFileContent[dateCode] = processedData
+    with open('ReactApp//optionsreplay//src//data//data.json','w') as fileHandle:
+        json.dump(dataFileContent, fileHandle, indent=2)
 
 for file in currentFiles:
     if file in currentJsonKeys:
         continue
     else:
-        jsonWrite(b3Reader.run(file))
+        jsonWrite(file, b3Reader.run(file))
